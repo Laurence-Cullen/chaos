0e57ce838ff592a552339e45733f4513dbe3da29
Laurence Cullen <laurencesimoncullen@gmail.com>
2018-04-22 12:14:01 +0100

    managed to fix batch size indexing problem

--- lstm.py
+++ lstm.py
@@ -7,6 +7,9 @@ import math
 import pandas as pd
 import mackey_glass
 
+# Developing general purpose LSTM model to predict the future values of univariate time series.
+# Drawn heavily from https://machinelearningmastery.com/time-series-forecasting-long-short-term-memory-network-python/
+
 
 def get_data():
     generator = mackey_glass.MackeyGlassGenerator()
@@ -21,21 +24,19 @@ def get_data():
 
 
 def fit_lstm(batch_size, train_data):
-    print(train_data.shape)
+    # TODO something wrong with array dimensions and batch_input_shape :'(
 
-    X = train_data.values[:, 0:-1]
-    X = X.reshape(X.shape[0], 1, X.shape[1])
+    print('train data shape = %s' % str(train_data.shape))
+    print(train_data)
 
-    y = train_data.values[:, -1]
-
-    print(X.shape)
-    print(y.shape)
+    x, y = train_data[:, 0:-1], train_data[:, -1]
+    x = x.reshape(x.shape[0], 1, x.shape[1])
 
     model = Sequential()
     model.add(LSTM(units=100,
               activation='tanh',
               stateful=True,
-              batch_input_shape=(batch_size, X.shape[1], X.shape[2])))
+              batch_input_shape=(batch_size, x.shape[1], x.shape[2])))
 
     model.add(Dense(1))
     model.compile(optimizer='adam', loss='mse')
@@ -43,23 +44,38 @@ def fit_lstm(batch_size, train_data):
     epochs = 50
 
     for epoch in range(0, epochs):
-        model.fit(x=X, y=y, epochs=1, batch_size=batch_size, verbose=1, shuffle=False)
+        model.fit(x=x, y=y, epochs=1, batch_size=batch_size, verbose=1, shuffle=False)
         model.reset_states()
 
     return model
 
 
 # make a one-step forecast
-def forecast_lstm(model, batch_size, X):
-    X = X.reshape(1, 1, len(X))
-    yhat = model.predict(X, batch_size=batch_size)
+def forecast_lstm(model, batch_size, x):
+    x = x.reshape(1, 1, len(x))
+    yhat = model.predict(x, batch_size=batch_size)
     return yhat[0, 0]
+
+
+# frame a sequence as a supervised learning problem
+def timeseries_to_supervised(data, lag=1):
+    df = pd.DataFrame(data)
+    columns = [df.shift(i) for i in range(1, lag+1)]
+    columns.append(df)
+    df = pd.concat(columns, axis=1)
+    df.fillna(0, inplace=True)
+    return df
 
 
 def main():
     train_series, test_series = get_data()
 
-    model = fit_lstm(batch_size=1, train_data=train_series)
+    print('train series shape = %s' % str(train_series.shape))
+
+    train_data = timeseries_to_supervised(train_series).values
+    test_data = timeseries_to_supervised(test_series).values
+
+    model = fit_lstm(batch_size=1, train_data=train_data)
 
     # forecast the entire training dataset to build up state for forecasting
     train_reshaped = train_series.values[:, 0:-1]
@@ -67,11 +83,12 @@ def main():
     model.predict(x=train_reshaped, batch_size=1)
 
     # walk-forward validation on the test data
-    predictions = list()
-    for i in range(test_series.shape[1]):
+    predictions = []
+
+    for i in range(test_data.shape[1]):
         # make one-step forecast
-        X, y = test_series.values[i, 0:-1], test_series.values[i, -1]
-        yhat = forecast_lstm(model, 1, X)
+        x, y = test_data.values[i, 0:-1], test_data.values[i, -1]
+        yhat = forecast_lstm(model, 1, x)
 
         # store forecast
         predictions.append(yhat)
